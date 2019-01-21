da3da48277ffc8115a5ae46e7c2d7182ae044665
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-21 18:46:51 +0000

    Deploy softprops/shiplift to github.com/softprops/shiplift.git:gh-pages

---
+++ source-files.js
@@ -0,0 +1,2 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex['shiplift'] = {"name":"","dirs":[],"files":["builder.rs","errors.rs","lib.rs","read.rs","rep.rs","tarball.rs","transport.rs","tty.rs"]};
